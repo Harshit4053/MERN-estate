5bada18d5d65aa59186d42e4d209b933163fba15
Harshit4053 <harshitgajipara204@gmail.com>
2025-04-19 15:44:49 +1000

    creat and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
-import SignIn from "./pages/Signin";
 import Header from "./components/Header";
+import Signin from "./pages/Signin";
 
 function App() {
   return(
@@ -12,7 +12,7 @@ function App() {
   <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-in" element={<Signin/>} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/about" element={<About />} />
