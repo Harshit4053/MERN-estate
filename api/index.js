47f0fa18862a2579925db599247815657e0b6103
Harshit4053 <harshitgajipara204@gmail.com>
2025-06-01 12:48:41 +1000

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express, { Router } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -19,10 +20,13 @@ mongoose
     console.error("❌ MongoDB connection error:", err);
   });
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
 });
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
  
