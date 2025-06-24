4a1b4319275be9ddc1d77b3fbf0fd1e0ae4a0fc5
Harshit4053 <harshitgajipara204@gmail.com>
2025-06-24 10:48:12 +1000

    Profile function updated

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,9 +2,10 @@ import express from 'express';
 import { test, updateUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/varifyUser.js';
 
+
 const router = express.Router();
 
 router.get('/test', test);
-router.post('/update/:id',verifyToken, updateUser)
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { updateUser } from "../../../api/controllers/user.controllers";
 
 const initialState = {
   currentUser: null,
@@ -7,7 +8,7 @@ const initialState = {
 };
 
 const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   reducers: {
     signInStart: (state) => {
@@ -22,8 +23,27 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} = userSlice.actions;
 export default userSlice.reducer;
