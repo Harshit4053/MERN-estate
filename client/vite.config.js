d0f352d3ee9c71dd1330d7f02fb8f56bff7ddb73
Harshit Gajipara <127908844+Harshit4053@users.noreply.github.com>
2025-07-17 15:18:14 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,25 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://mern-estate-eta-eight.vercel.app",
-        changeOrigin: true,
-        secure: false,
-        configure: (proxy, options) => {
-          proxy.on('error', (err, req, res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
-  },
   plugins: [react(), tailwindcss()],
 });
